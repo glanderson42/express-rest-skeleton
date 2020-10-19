462589f9fda607113ceb418379cb495aef07fda2
glanderson42 <glanderson42@protonmail.com>
2020-10-19 17:51:04 +0200

    add config with convict

---
+++ src/config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const convict = require('convict');
+
+const config = convict({
+  env: {
+    doc: 'Environment var',
+    format: ['production', 'development'],
+    default: 'development',
+    env: 'NODE_ENV',
+  },
+  port: {
+    doc: 'The port what the application used',
+    format: Number,
+    default: 5000,
+    env: 'PORT',
+  },
+  middleware: {
+    secret: {
+      doc: 'The secret used in jwt',
+      format: String,
+      default: 'Secret',
+      env: 'SECRET',
+    },
+  },
+});
